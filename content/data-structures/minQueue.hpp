19a3da2e8a40eb299799db371bce284fe6ca8446
Noobgam <shessmaster12@gmail.com>
2019-04-05 20:48:28 +0000

    minQueue done

--- content/data-structures/minQueue.hpp
+++ content/data-structures/minQueue.hpp
@@ -12,15 +12,77 @@ namespace olymplib {
 
     template <class T>
     struct FunctorQueue<T, NPrivate::Minner> {
-        constexpr static size_t DEFAULT_SIZE = 4;
+        constexpr static size_t DEFAULT_SIZE = 1;
 
-        std::pair<T, size_t>* _data = (std::pair<int, size_t>*)malloc(sizeof(std::pair<T, size_t>) * DEFAULT_SIZE);
+        std::pair<T, size_t>* _data;
+        
+        FunctorQueue() : FunctorQueue(DEFAULT_SIZE) {
+        }
+
+        FunctorQueue(size_t t) {
+            _data = (std::pair<T, size_t>*)malloc(sizeof(std::pair<T, size_t>) * DEFAULT_SIZE);
+        }
 
         size_t l = 0, r = 0, _b = 0, _e = 0, _size = DEFAULT_SIZE;
 
-        void push(const T& t)
+        void push(const T& t) {
+            while (_b != _e && _back() >= t) {
+                _pop_back();
+            }
+            _push_min(t);
+            r++;
+        }
+
+        void pop() {
+            if (_data[_b].second == l++) {
+                _pop_min();
+            }
+        }
 
         ~FunctorQueue() { free(_data); }
+
+        void _pop_min() {
+            _b++;
+            if (_b == _size) _b = 0;
+        }
+
+        T& _back() {
+            size_t x = _e;
+            if (x == 0) {
+                x = _size;
+            }
+            x--;
+            return _data[x].first;
+        }
+
+        void _pop_back() {
+            if (_e == 0) {
+                _e = _size;
+            }
+            _e--;
+        }
+
+        void _push_min(const T& t) {
+            _data[_e++] = {t, r};
+            if (_e == _size) _e = 0;
+            if (_b == _e) {
+                std::pair<T, size_t>* newData = (std::pair<T, size_t>*)malloc(sizeof(std::pair<T, size_t>) * 2 * _size);
+                for (int i = 0; i < _size; ++i) {
+                    newData[i] = std::move(_data[_b]);
+                    _pop_min();
+                }
+                _b = 0;
+                _e = _size;
+
+                _size <<= 1;
+                swap(newData, _data);
+                free(newData);
+            }
+        }
+
+        T& get_min() {
+            return _data[_b].first;
+        }
     };
 
     template <class T>
--- tests/data-structures/minQueue.cpp
+++ tests/data-structures/minQueue.cpp
@@ -1,20 +1,140 @@
 #include "gtest/gtest.h"
 #include "data-structures/minQueue.hpp"
+#include <random>
+#include <set>
+#include <queue>
 
 using namespace olymplib;
+using namespace std;
 
 TEST(correctness, pushpop) {
     MinQueue<int> a;
-    a.push(1);
-    EXPECT_EQ(a.getMin(), 1);
     a.push(2);
-    EXPECT_EQ(a.getMin(), 1);
-    a.push(2);
-    EXPECT_EQ(a.getMin(), 1);
+    EXPECT_EQ(a.get_min(), 2);
+    a.push(4);
+    EXPECT_EQ(a.get_min(), 2);
     a.pop();
-    EXPECT_EQ(a.getMin(), 2);
+    EXPECT_EQ(a.get_min(), 4);
+    a.push(3);
+    EXPECT_EQ(a.get_min(), 3);
     a.pop();
-    EXPECT_EQ(a.getMin(), 2);
-    a.push(1);
-    EXPECT_EQ(a.getMin(), 1);
+    EXPECT_EQ(a.get_min(), 3);
+    a.pop();
 }
+
+TEST(correctness, fuzzy_pushpop) {
+    const int ITER = 1e4;
+    for (int seed = 0; seed < 10; ++seed) {
+        srand(seed);
+        MinQueue<int> a;
+        multiset<int> ss;
+        queue <int> q;
+        for (int i = 0; i < ITER; ++i) {
+            int type = 0;
+            if (ss.size() != 0) type = rand() % 2;
+            if (type == 0) {
+                int val = rand();
+                ss.insert(val);
+                q.push(val);
+                a.push(val);
+            } else {
+                int front = q.front();
+                a.pop();
+                ss.erase(ss.lower_bound(front));
+                q.pop();
+            }
+            if (ss.size() != 0) {
+                EXPECT_EQ(*ss.begin(), a.get_min()) << " on iteration " << i << " seed " << seed;
+            }
+        }
+    }
+}
+
+TEST(correctness, fuzzy_pushpop_reserve) {
+    const int ITER = 1e4;
+    for (int seed = 0; seed < 10; ++seed) {
+        srand(seed);
+        MinQueue<int> a(ITER / 2);
+        multiset<int> ss;
+        queue <int> q;
+        for (int i = 0; i < ITER; ++i) {
+            int type = 0;
+            if (ss.size() != 0) type = rand() % 2;
+            if (type == 0) {
+                int val = rand();
+                ss.insert(val);
+                q.push(val);
+                a.push(val);
+            } else {
+                int front = q.front();
+                a.pop();
+                ss.erase(ss.lower_bound(front));
+                q.pop();
+            }
+            if (ss.size() != 0) {
+                EXPECT_EQ(*ss.begin(), a.get_min()) << " on iteration " << i << " seed " << seed;
+            }
+        }
+    }
+}
+
+const int STRESS_ITER = 1e8;
+TEST(stress, fuzzy_pushpop) {
+    srand(228);
+    MinQueue<int> a;
+    size_t sz = 0;
+    for (int i = 0; i < STRESS_ITER; ++i) {
+        int type = 0;
+        if (sz != 0) 
+            type = rand() % 2;
+        if (type == 0) {
+            int val = rand();
+            a.push(val);
+            ++sz;
+        } else {
+            a.pop();
+            --sz;
+        }
+    }
+}
+
+TEST(stress, fuzzy_pushpop_stl) {
+    srand(228);
+    size_t sz = 0;
+    multiset<int> ss;
+    queue <int> q;
+    for (int i = 0; i < STRESS_ITER; ++i) {
+        int type = 0;
+        if (ss.size() != 0) type = rand() % 2;
+        if (type == 0) {
+            int val = rand();
+            ss.insert(val);
+            q.push(val);
+        } else {
+            int front = q.front();
+            ss.erase(ss.lower_bound(front));
+            q.pop();
+        }
+    }
+}
+
+TEST(stress, fuzzy_pushpop_reserve) {
+    srand(228);
+    MinQueue<int> a(STRESS_ITER / 2);
+    size_t sz = 0;
+    for (int i = 0; i < STRESS_ITER; ++i) {
+        int type = 0;
+        if (sz != 0) 
+            type = rand() % 2;
+        if (type == 0) {
+            int val = rand();
+            a.push(val);
+            ++sz;
+        } else {
+            a.pop();
+            --sz;
+        }
+    }
+}
+
+
